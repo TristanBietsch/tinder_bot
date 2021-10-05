27bfbbd69bb64475716a75fbc352a167cb6e5a46
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-10-05 15:23:25 -0500

    added window issue fix

---
+++ tinder_bot.py
@@ -0,0 +1,25 @@
+from selenium import  webdriver
+from time import sleep
+
+class TinderBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        
+    def login(self):
+        self.driver.get('https://tinder.com')
+        
+        sleep(3)
+         
+        self.driver.find_element_by_xpath('//*[@id="q-600306533"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')   
+        
+        login_btn = self.driver.find_element_by_xpath('//*[@id="q-600306533"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')   
+        login_btn.click()
+        
+        fb_btn = self.driver.find_element_by_xpath('//*[@id="q1654454959"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+        fb_btn.click()
+        
+        
+        
+        #switch to login popup
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to_window(self.driver.window_handles[1])
