6752c49ca023e332268fb7c67f61742d21ce8dcb
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-10-05 16:43:54 -0500

    like and auto swipe functions done

--- tinder_bot.py
+++ tinder_bot.py
@@ -40,8 +40,50 @@ class TinderBot():
         pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
         pw_in.send_keys(password)
         
-        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0_Z3"]')
+        #login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0_Z3"]')
+        login_btn = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
         login_btn.click()
+        
+        sleep(2)
+        
+        self.driver.switch_to_window(base_window)
+        
+        sleep(2)
+        
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="q1654454959"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+        
+        sleep(2)
+        
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="q1654454959"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+        
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn.click()
+
+    def auto_swipe(self):
+        while True:
+            sleep(0.5)
+            try:
+                self.like()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
+
+    def close_popup(self):
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3.click()
+
+    def close_match(self):
+        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup.click()   
         
         
 bot = TinderBot()
